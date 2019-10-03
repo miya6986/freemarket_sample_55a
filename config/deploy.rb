1f556b86d729d9d9dad6fc936296d723fa3e06f5
miya6986 <miyadev49802@gmail.com>
2019-10-03 13:36:12 +0900

    Correct deploy.rb
    - Activate "linked_file" description
    - Add "master.key" to "linked_file" description

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,11 +21,11 @@ set :repo_url, "git@github.com:me/miya6986/freemarket_sample_55a.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'config/master.key')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
