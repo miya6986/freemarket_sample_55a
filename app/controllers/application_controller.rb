d1f39bf61be216c532ced90c3c8580a060e989b4
miya6986 <miyadev49802@gmail.com>
2019-10-04 10:51:43 +0900

    Correct basic authenticate setting
    - credentialsの読み込みの表記修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials.basic_auth[:user] && password == Rails.application.credentials.basic_auth[:password]
+      username == Rails.application.credentials.basic_auth_user && password == Rails.application.credentials.basic_auth_password
     end
   end
 end
