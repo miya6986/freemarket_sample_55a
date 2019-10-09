a90f3ebcbacefc8b774dd34b123b5a6dcc11b524
miya6986 <miyadev49802@gmail.com>
2019-10-09 20:02:53 +0900

    identification HTML

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def index    
   end
+
+  def user_identification
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'products#index'
-  resources :users,only: 'index'
+  resources :users,only: 'index' do
+    collection do
+      get :user_identification
+    end
+  end
   get 'step1', to: 'registrations#step1'
   get 'step2', to: 'registrations#step2'
   get 'step3', to: 'registrations#step3'
