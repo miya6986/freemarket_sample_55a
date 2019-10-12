01a803c47bbd3c9f081a32df347b8b6cbb121c89
yutaro704 <punpunsaori@yahoo.co.jp>
2019-10-12 23:03:03 +0900

    ルーティングの記述の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
+ルーティング
+
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :users,only: 'index'
-  get 'step1', to: 'registrations#step1'
-  get 'step2', to: 'registrations#step2'
-  get 'step3', to: 'registrations#step3'
-  get 'step4', to: 'registrations#step4'
-  get 'step5', to: 'registrations#step5'
-  get 'step6', to: 'registrations#step6'
-  get 'login', to: 'registrations#login'
+  
+  resources :registrations do
+    collection do
+      get  'step2'
+      get  'step3'
+      get  'step4'
+      get  'step5'
+      post 'step6'
+    end
+  end
 end
+
