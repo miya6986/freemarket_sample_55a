d8a9c4ab09544ef2d100b9a276b57620f6ef5878
yutaro704 <punpunsaori@yahoo.co.jp>
2019-10-12 22:21:24 +0900

    active-hashの導入

---
+++ db/migrate/20191012131907_create_prefectures.rb
@@ -0,0 +1,11 @@
+class CreatePrefectures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :prefectures do |t|
+      t.integer :prefecture_id
+      t.string  :city
+
+      t.timestamps
+    end
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_130824) do
+ActiveRecord::Schema.define(version: 2019_10_12_131907) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address_firstname", null: false
