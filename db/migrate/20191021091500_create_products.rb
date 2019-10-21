57d44f0a0a83fbe7365297893887a6e8f233213b
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-10-21 20:53:21 +0900

    source_areaをprefecture_idに変更
    配送元地域はactive_hashを用いるため

--- db/migrate/20191021091500_create_products.rb
+++ db/migrate/20191021091500_create_products.rb
@@ -1,14 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.integer :prefecture_id
       t.string :name,            null: false, index: true
       t.text :description,       null: false
       t.string :size
       t.string :condition,       null: false
       t.string :postage,         null: false
       t.string :shipping_method, null: false
-      t.string :source_area,     null: false
+      t.integer :prefecture_id,  null: false
       t.string :shipping_days,   null: false
       t.integer :price,          null: false
       t.references :buyer,    foreign_key: { to_table: :users }
