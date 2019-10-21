a967d106b6dbc5a51c0b3e2d0c9498d029a0f775
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-10-21 18:20:30 +0900

    migrationファイルにproductテーブルのカラムを記述
    今後実装していく上で必要と考えられるカラムを記述。

--- db/migrate/20191021091500_create_products.rb
+++ db/migrate/20191021091500_create_products.rb
@@ -1,7 +1,20 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      
+      t.integer :prefecture_id
+      t.string :name,            null: false, index: true
+      t.text :description,       null: false
+      t.string :size
+      t.string :condition,       null: false
+      t.string :postage,         null: false
+      t.string :shipping_method, null: false
+      t.string :source_area,     null: false
+      t.string :shipping_days,   null: false
+      t.integer :price,          null: false
+      t.references :buyer,    foreign_key: { to_table: :users }
+      t.references :seller,   foreign_key: { to_table: :users }
+      # t.references :category, foreign_key: true
+      # t.references :bland,    foreign_key: true
 
       t.timestamps
     end
