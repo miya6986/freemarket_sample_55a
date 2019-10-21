de620ab61c73a7f20e7aa7c8589840b5deb9d335
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-10-21 18:41:08 +0900

    migrationファイルにsns_credentialのカラムを追加

--- db/migrate/20191021093738_create_sns_credentials.rb
+++ db/migrate/20191021093738_create_sns_credentials.rb
@@ -1,6 +1,9 @@
 class CreateSnsCredentials < ActiveRecord::Migration[5.2]
   def change
     create_table :sns_credentials do |t|
+      t.string :uid
+      t.string :provider
+      t.references :user,     foreign_key: true, null: false
 
       t.timestamps
     end
