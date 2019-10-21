98eea62f6c4c815cde4d4aef5a251406e111a932
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-10-21 18:14:27 +0900

    migrationファイルにuserテーブルのカラムを記述
    開発していく上で必要と考えられるカラムを全て記述。

--- db/migrate/20191021090347_devise_create_users.rb
+++ db/migrate/20191021090347_devise_create_users.rb
@@ -4,8 +4,25 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :nickname,                   null: false
+      t.string  :email,                      null: false, unique: true, default: ""
+      t.string  :firstname,                  null: false 
+      t.string  :lastname,                   null: false
+      t.string  :firstname_kana,             null: false
+      t.string  :lastname_kana,              null: false
+      t.integer :birth_year,                 null: false
+      t.integer :birth_month,                null: false
+      t.integer :birth_day,                  null: false
+      t.string  :phone_number,               null: false, unique: true
+      t.string  :encrypted_password,         null: false, default: ""
+      t.integer :postalcode
+      t.integer :prefecture_id
+      t.string  :city_name
+      t.string  :address_number
+      t.string  :building_name
+      t.string  :gender
+      t.string  :avatar
+      t.text    :profile
 
       ## Recoverable
       t.string   :reset_password_token
