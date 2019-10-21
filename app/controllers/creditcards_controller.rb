1058b951672675c754b649eba05f11e93031daeb
nousi <nousidayo@gmail.com>
2019-10-21 12:02:09 +0900

    顧客情報の取得
    Payjp.api_key = テスト用の秘密鍵を環境変数で呼び出す。
    customer = Payjp::Customer.retrieve(顧客ID)で顧客情報を呼び出す
    
    参考ページ
    https://pay.jp/docs/api/?ruby#%E9%A1%A7%E5%AE%A2%E3%82%92%E4%BD%9C%E6%88%90

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -2,6 +2,13 @@ class CreditcardsController < ApplicationController
   def index
   end
 
+  def show
+    creditcard = Creditcard.where(user_id: current_user.id)
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    customer = Payjp::Customer.retrieve(card.customer_id)
+    
+  end
+
   def destroy    
   end
 end
