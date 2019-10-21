9051343d6137c6eab0dde28c959560f7a2530c11
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-10-21 18:47:31 +0900

    migrationファイルのカラム名修正
    外部キーカラムを作る場合migrationファイルのカラム名に_idの記述は不要

--- db/migrate/20191021093100_create_images.rb
+++ db/migrate/20191021093100_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.string :name,              null: false
-      t.references :product_id,    foreign_key: true, null: false
+      t.references :product,    foreign_key: true, null: false
 
       t.timestamps
     end
