c9f2e327f9524ae6402c426934e32e5914875a10
nousi <nousidayo@gmail.com>
2019-10-21 12:57:50 +0900

    new createの記述を実施
    newでcardを定義する。
    createのelse下でpayjpとcreditcardテーブルの新規作成を実施
     　customer = Payjp::Customer.createでPayjpに登録する情報
     　@card = Creditcard.new でcreditcardテーブルに保存する情報
    保存をしたら、indexに戻す。indexはmypage上のcard情報となっている。

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -5,12 +5,36 @@ class CreditcardsController < ApplicationController
   end
 
   def show
-    creditcard = Creditcard.where(user_id: current_user.id).first
+    card = Creditcard.where(user_id: current_user.id).first
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    customer = Payjp::Customer.retrieve(creditcard.customer_id)
-    @card_info = customer.cards.retrieve(creditcard.card_id)
+    customer = Payjp::Customer.retrieve(card.customer_id)
+    @card_info = customer.cards.retrieve(card.card_id)
   end
 
-  def destroy    
+  def new
+    card = Creditcard.where(user_id: current_user.id).first
+    redirect_to action: "index" if card.exists?    
+  end
+
+  def create
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+      email: current_user.email,
+      card: params['payjp-token'],
+      metadata: {user_id: current_user.id}
+      )
+      @card = Creditcard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: "index"
+      else
+        redirect_to action: "create"
+      end
+    end
+  end
+
+  def destroy     
   end
 end
