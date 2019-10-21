2e49a669c22782273cc42ea981ceb40957654826
nousi <nousidayo@gmail.com>
2019-10-21 12:21:13 +0900

    payjpの呼び出し require"payjp"
    controllerにpayjpを呼び出す必要があるため、require"payjp"を追記
    
    一部cardとして記述している部分があったが、creditcardで定義をしていたため、cardをcreditcardに変更した

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,12 +1,14 @@
 class CreditcardsController < ApplicationController
+  require "payjp"
+
   def index
   end
 
   def show
-    creditcard = Creditcard.where(user_id: current_user.id)
+    creditcard = Creditcard.where(user_id: current_user.id).first
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    customer = Payjp::Customer.retrieve(card.customer_id)
-    
+    customer = Payjp::Customer.retrieve(creditcard.customer_id)
+    @card_info = customer.cards.retrieve(creditcard.card_id)
   end
 
   def destroy    
