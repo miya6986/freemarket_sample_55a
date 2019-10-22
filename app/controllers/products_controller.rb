47c3f02fc73556dac6845f94134faca67806337b
KengoNagata <majorboy47@gmail.com>
2019-10-22 16:09:46 +0900

    商品出品→new /createアクションの実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,32 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to users_path, notice: "商品を出品しました"
+    else 
+      render 'new'
+    end
+  end
+
   def buy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category,
+      :size,
+      :condition,
+      :postage,
+      :shipping_method,
+      :prefecture_id,
+      :shipping_days,
+      :price
+    )
   end
   
 end
