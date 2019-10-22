b49ba60c4a314d0bc4cbab75e200c39fbc935cc4
KengoNagata <majorboy47@gmail.com>
2019-10-22 16:10:49 +0900

    アソシエーションを一通りコメントアウト
    目的：取り急ぎProductモデルでの登録機能を実装するため

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,15 @@
 class Product < ApplicationRecord
-  belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
-  belongs_to :user
-  has_many :images, dependent: :destroy
-  belongs_to :category
-  belongs_to :brand
+  # belongs_to :seller, class_name: "User"
+  # belongs_to :buyer, class_name: "User"
+  # belongs_to :user
+  # has_many :images, dependent: :destroy
+  # belongs_to :category
+  # belongs_to :brand
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 end
+
+
+
+
+
