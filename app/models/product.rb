b6c0b40034df8ce9540ec2e01be49a59c4f25901
KengoNagata <majorboy47@gmail.com>
2019-10-22 17:03:14 +0900

    カテゴリ登録欄のコメントアウトを解除
    目的：商品出品機能のカテゴリ登録を有効化する為

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   # belongs_to :buyer, class_name: "User"
   # belongs_to :user
   # has_many :images, dependent: :destroy
-  # belongs_to :category
+  belongs_to :category
   # belongs_to :brand
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
