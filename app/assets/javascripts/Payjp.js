f1a51beccaa988bb9d018367e97cf868bc0dddac
nousi <nousidayo@gmail.com>
2019-10-23 13:35:42 +0900

    公式を参考に、基本的なJSの記述を完了させる
    Payjpの公式ガイドページの下記ページに載っているを参考に修正
    カード情報のトークン化
    https://pay.jp/docs/cardtoken

--- app/assets/javascripts/Payjp.js
+++ app/assets/javascripts/Payjp.js
@@ -1,28 +1,37 @@
 $(document).on('turbolinks:load',function(){
-  var form = $("");
   Payjp.setPublicKey('');
-  $(document).on("click", "#submit-button", function(e) {
+  var form = $("#charge-form"),
+      number = form.find('input[name="number"]'),
+      cvc = form.find('input[name="cvc"]'),
+      exp_month = form.find('select[name="exp_month"]'),
+      exp_year = form.find('input[name="exp_year"]');
 
-    e.preventDefault();
+
+  $("#charge-form").submit(function() {
+    form.find("input[type=submit]").prop("disabled", true);
 
     var card = {
-      number: $("").val();
-      cvc: $("").val();
-      exp_month: $("").val();
-      exp_year: $("").val();
+      number: $("").val(),
+      cvc: $("").val(),
+      exp_month: $("").val(),
+      exp_year: $("").val(),
     };
+
     Payjp.createToken(card, function(status, response) {
       if (response.error){
-        alert('トークン作成エラー発生')
-      }
+        form.find('.payment-errors').text(response.error.message);
+        form.find('button').prop('disabled', false);
+      }   
       else {
         $(".numver").removeAttr("name");
         $(".cvc").removeAttr("name");
         $(".exp_month").removeAttr("name");
         $(".exp_year").removeAttr("name");
-      }
-    }
-  });
-  
+        var token = response.id;
 
+        form.append($('<input type="hidden" name="payjpToken" />').val(token));
+        form.get(0).submit();
+      }
+    };
+  });
 });
