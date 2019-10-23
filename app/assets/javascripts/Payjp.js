40524da6b13c475d8c6ce5ee1ecfb684ee6459d0
nousi <nousidayo@gmail.com>
2019-10-23 13:18:02 +0900

    Update Payjp.js
      自分のサーバーにはPOSTしないように、.removeAtter()で情報を削除
            $(".numver").removeAttr("name");
            $(".cvc").removeAttr("name");
            $(".exp_month").removeAttr("name");
            $(".exp_year").removeAttr("name");

--- app/assets/javascripts/Payjp.js
+++ app/assets/javascripts/Payjp.js
@@ -11,6 +11,17 @@ $(document).on('turbolinks:load',function(){
       exp_month: $("").val();
       exp_year: $("").val();
     };
+    Payjp.createToken(card, function(status, response) {
+      if (response.error){
+        alert('トークン作成エラー発生')
+      }
+      else {
+        $(".numver").removeAttr("name");
+        $(".cvc").removeAttr("name");
+        $(".exp_month").removeAttr("name");
+        $(".exp_year").removeAttr("name");
+      }
+    }
   });
   
 
