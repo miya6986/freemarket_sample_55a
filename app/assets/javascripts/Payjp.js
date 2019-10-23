e548fa4556e3e5304c03b112e7d24d5371161030
nousi <nousidayo@gmail.com>
2019-10-23 18:12:08 +0900

    form_withに合わせて、Payjpの記述にidにする
    form_withで作成したinputはid名が付与されてるため、下記の#を付与した
        var card = {
          number: $("#card_number").val(),
          cvc: $("#cvc").val(),
          exp_month: $("#exp_month").val(),
          exp_year: $("#exp_year").val(),
        };
    
            $("#card_number").removeAttr("name");
            $("#cvc").removeAttr("name");
            $("#exp_month").removeAttr("name");
            $("#exp_year").removeAttr("name");

--- app/assets/javascripts/Payjp.js
+++ app/assets/javascripts/Payjp.js
@@ -11,27 +11,28 @@ $(document).on('turbolinks:load',function(){
     form.find("input[type=submit]").prop("disabled", true);
 
     var card = {
-      number: $("").val(),
-      cvc: $("").val(),
-      exp_month: $("").val(),
-      exp_year: $("").val(),
+      number: $("#card_number").val(),
+      cvc: $("#cvc").val(),
+      exp_month: $("#exp_month").val(),
+      exp_year: $("#exp_year").val(),
     };
 
     Payjp.createToken(card, function(status, response) {
+
       if (response.error){
         form.find('.payment-errors').text(response.error.message);
         form.find('button').prop('disabled', false);
       }   
       else {
-        $(".numver").removeAttr("name");
-        $(".cvc").removeAttr("name");
-        $(".exp_month").removeAttr("name");
-        $(".exp_year").removeAttr("name");
+        $("#card_number").removeAttr("name");
+        $("#cvc").removeAttr("name");
+        $("#exp_month").removeAttr("name");
+        $("#exp_year").removeAttr("name");
         var token = response.id;
-
+        
         form.append($('<input type="hidden" name="payjpToken" />').val(token));
         form.get(0).submit();
-      }
-    };
+      };
+    });
   });
 });
