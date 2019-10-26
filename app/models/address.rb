55ee192d44325ca94be209babf4ebd3ef73983c3
yutaro704 <yutaro.704@gmail.com>
2019-10-26 16:41:55 +0900

    address_model,controller,viewファイルの編集
    ・addressモデルの変更
    カラム名address_〇〇の「address_」を削除
    
    ・controllerの編集
    address_〇〇の「address_」を削除
    
    ・viewファイルの編集
    address_〇〇の「address_」を削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,17 @@
 class Address < ApplicationRecord
+  belongs_to :user
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  validates     :firstname,          presence: true, length: { maximum: 35 }
+  validates     :lastname,           presence: true, length: { maximum: 35 }
+  validates     :firstname_kana,     presence: true, length: { maximum: 35 }
+  validates     :lastname_kana,      presence: true, length: { maximum: 35 }
+  validates     :postalcode,                 presence: true
+  validates     :prefecture_id,              presence: true
+  validates     :city_name,                  presence: true, length: { maximum: 50 }
+  validates     :address_number,             presence: true, length: { maximum: 100 }
+  validates     :building_name,              length: { maximum: 100 }
+  validates     :phone_number,       length: { maximum: 100 }
 end
