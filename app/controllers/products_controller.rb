c928437909794fb97ed3fc91bf42ca3ac28f8d9d
yutaro704 <yutaro.704@gmail.com>
2019-10-26 16:11:04 +0900

    showアクションにインスタンス変数を定義#1
    showアクションにインスタンス変数を定義し、viewを変更。
    アイテム名       @name
    ブランド　       @brand
    状態              　@condition
    配送料の負担　@postage
    配送の方法　    @shipping_method
    配送の目安    　@shipping_days
    値段              　@price
    商品の説明　   @detail

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,5 +7,30 @@ class ProductsController < ApplicationController
 
   def buy
   end
+
+  def show
+    # トップページから詳細ページへの移管を行う
+    @product = Product.find(params[:id])
+    # 出品者
+    # @seller
+    # アイテム名
+    @name = @product.name
+    # カテゴリー
+    # ブランド
+    @brand = @product.brand.name
+    # 状態
+    @condition = @product.condition
+    # 配送料の負担
+    @postage = @product.postage
+    # 配送の方法
+    @shipping_method = @product.shipping_method
+    # 配送元の地域
+    # 配送の目安
+    @shipping_days = @product.shipping_days
+    # 値段
+    @price = @product.price
+    # 商品の説明
+    @detail = @product.description
+  end
   
 end
