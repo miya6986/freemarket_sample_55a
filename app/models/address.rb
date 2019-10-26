3b4cd5750ba8c5262b773bf477bb4d9117ff5bd7
yutaro704 <yutaro.704@gmail.com>
2019-10-26 10:46:12 +0900

    addressモデルの削除と再作成
    カラム名変更の為、addressテーブルの削除と再作成を行う

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,17 +1,2 @@
 class Address < ApplicationRecord
-  belongs_to :user
-  
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-
-  validates     :address_firstname,          presence: true, length: { maximum: 35 }
-  validates     :address_lastname,           presence: true, length: { maximum: 35 }
-  validates     :address_firstname_kana,     presence: true, length: { maximum: 35 }
-  validates     :address_lastname_kana,      presence: true, length: { maximum: 35 }
-  validates     :postalcode,                 presence: true
-  validates     :prefecture_id,              presence: true
-  validates     :city_name,                  presence: true, length: { maximum: 50 }
-  validates     :address_number,             presence: true, length: { maximum: 100 }
-  validates     :building_name,              length: { maximum: 100 }
-  validates     :address_phone_number,       length: { maximum: 100 }
 end
