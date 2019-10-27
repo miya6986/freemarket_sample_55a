4545fca1e9c85e52908ab2e9b617006bdb8cb3ac
yutaro704 <yutaro.704@gmail.com>
2019-10-27 09:49:49 +0900

    addressモデルのテストの記述を変更
    addressモデルのカラム変更に伴い、adressモデルのテストの記述を変更した。
    テストの作動も確認。

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
 
   factory  :address do
-    address_firstname          {"mercari"}
-    address_lastname           {"taro"}
-    address_firstname_kana     {"mercari"}
-    address_lastname_kana      {"taro"}
+    firstname                  {"mercari"}
+    lastname                   {"taro"}
+    firstname_kana             {"mercari"}
+    lastname_kana              {"taro"}
     postalcode                 {"1111111"}
     prefecture_id              {"1"}
     city_name                  {"city"}
     address_number             {"1"}
     building_name              {"mercari"}
-    address_phone_number       {"11111111111"}
+    phone_number               {"11111111111"}
     
     association :user
   end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -3,33 +3,33 @@ require 'rails_helper'
 describe Address do
   describe '#create' do
     # 1. アドレスモデルのお届け先・発送先住所の氏名、氏名カナ、郵便番号、住所があれば登録ができること
-    it "is valid with a address_firstname, address_lastname, address_firstname_kana, address_lastname_kana, postalcode, prefecture_id, city_name, address_number" do
+    it "is valid with a firstname, lastname, firstname_kana, lastname_kana, postalcode, prefecture_id, city_name, address_number" do
       address = build(:address)
       expect(address).to be_valid
     end
     # 2. 発送先氏は空では登録できないこと
-    it "is invalid without a address_firstname" do
-      address = build(:address, address_firstname: nil)
+    it "is invalid without a firstname" do
+      address = build(:address, firstname: nil)
       address.valid?
-      expect(address.errors[:address_firstname]).to include("を入力してください")
+      expect(address.errors[:firstname]).to include("を入力してください")
     end
     # 3. 発送先名は空では登録できないこと
-    it "is invalid without a address_firstname" do
-      address = build(:address, address_lastname: nil)
+    it "is invalid without a firstname" do
+      address = build(:address, lastname: nil)
       address.valid?
-      expect(address.errors[:address_lastname]).to include("を入力してください")
+      expect(address.errors[:lastname]).to include("を入力してください")
     end
     # 4. 発送先氏カナは空では登録できないこと
-    it "is invalid without a address_firstname" do
-      address = build(:address, address_firstname_kana: nil)
+    it "is invalid without a firstname" do
+      address = build(:address, firstname_kana: nil)
       address.valid?
-      expect(address.errors[:address_firstname_kana]).to include("を入力してください")
+      expect(address.errors[:firstname_kana]).to include("を入力してください")
     end
     # 5. 発送先名カナは空では登録できないこと
-    it "is invalid without a address_lastname_kana" do
-      address = build(:address, address_lastname_kana: nil)
+    it "is invalid without a lastname_kana" do
+      address = build(:address, lastname_kana: nil)
       address.valid?
-      expect(address.errors[:address_lastname_kana]).to include("を入力してください")
+      expect(address.errors[:lastname_kana]).to include("を入力してください")
     end
     # 6. 郵便番号は空では登録できないこと
     it "is invalid without a postalcode" do
@@ -74,14 +74,14 @@ describe Address do
       expect(address).to be_valid
     end
     # 13. 電話が101文字以上であれば登録できないこと
-    it "is invalid without a address_phone_number that has more than 101 characters " do
-      address = build(:address, address_phone_number: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    it "is invalid without a phone_number that has more than 101 characters " do
+      address = build(:address, phone_number: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
       address.valid?
-      expect(address.errors[:address_phone_number][0]).to include("は100文字以内で入力してください")
+      expect(address.errors[:phone_number][0]).to include("は100文字以内で入力してください")
     end
     # 14. 電話が100文字以下であれば登録できること
-    it "is valid with a address_phone_number that has less than 100 characters " do
-      address = build(:address, address_phone_number: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    it "is valid with a phone_number that has less than 100 characters " do
+      address = build(:address, phone_number: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
       address.valid?
       expect(address).to be_valid
     end
