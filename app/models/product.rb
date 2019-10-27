6561833be99d0eb7c9d90ec058bdedd14ddecac2
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-10-27 15:39:18 +0900

    productモデルのbelongs_to :userを削除
    アソシエーション上、sellerとbuyerがあれば足りるためbelongs_to :userを削除した。

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
-  belongs_to :user
   has_many :images, dependent: :destroy
   belongs_to :category
   belongs_to :brand
