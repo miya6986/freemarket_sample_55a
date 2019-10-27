b9b64a4e09ae22397d978a748917541b67193e55
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-10-27 17:12:21 +0900

    ページ内リンクのスムーススクロール
    inner_link.jsを作成。
    トップページにカテゴリー及びブランドのリンクを記述し、飛びたい場所にidを指定し、実装した。
    
    なお遷移先のポジションや遷移スピードを本家に近づけるために調整した。

---
+++ app/assets/javascripts/inner_link.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('a[href^="#"]').click(function() {
+    var href= $(this).attr("href");
+    var hash = href == "#" || href == "" ? 'html' : href;
+    scrollToAnker(hash);
+    return false;
+  });
+  
+  function scrollToAnker(hash) {
+    var target = $(hash);
+    var headerHight = 20; // ヘッダーの高さを入れる
+    var position = target.offset().top-headerHight;
+    console.log('position');
+    $('body,html').stop().animate({scrollTop:position}, 250);
+  }
+})
