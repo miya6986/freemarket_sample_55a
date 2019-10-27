a8189c5232c973ec398955db9693f95a092b6b23
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-10-27 18:34:25 +0900

    アソシエーションをコメントアウト
    user関連の機能が実装前なのでモデル関連のエラーが出る可能性があるため現段階ではコメントアウト

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  belongs_to :seller, class_name: "User", foreign_key: "seller_id"
-  belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
+  # belongs_to :seller, class_name: "User", foreign_key: "seller_id"
+  # belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
   has_many :images, dependent: :destroy
   belongs_to :category
   belongs_to :brand
