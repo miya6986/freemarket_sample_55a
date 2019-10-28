0697bbec5c1be67c4d48311c10de6cb34088f0a4
nousi <nousidayo@gmail.com>
2019-10-28 22:33:10 +0900

    一部トラッシュで修正

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -27,35 +27,29 @@ class CreditcardsController < ApplicationController
   end
 
   def new
-    card = Creditcard.where(user_id: current_user.id).first
-    redirect_to action: "index" if card.exists?    
+    @card = Creditcard.where(user_id: current_user.id).first
+    redirect_to action: "index" if @card.present?    
   end
 
   def create
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     if params['payjp-token'].blank?
-      redirect_to action: "new"
+      redirect_to action: "index"
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.create(
-      email: current_user.email,
-      card: params['payjp-token'],
-      metadata: {user_id: current_user.id}
+        description: 'test',
+        email: current_user.email,
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
       )
       @card = Creditcard.new(user_id: current_user.id, customer_id: customer.id)
       if @card.save
         redirect_to action: "index"
       else
-        redirect_to action: "create"
+        redirect_to action: "new"
       end
     end
   end
-
-  def show
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    customer = Payjp::Customer.retrieve(@card.customer_id)
-    @card_info = customer.cards.retrieve(@card.customer_id.default_card)
-  end
-
 
   def destroy     
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
