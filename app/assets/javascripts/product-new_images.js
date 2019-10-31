23cb1d935c172a4144439b1c3c064d947a57d1ed
nousi <nousidayo@gmail.com>
2019-10-31 22:25:00 +0900

    プレビュー機能を一から作り直す
    = image.file_fieldにlass: "image_upload"を付与
    
    image_uploadに画像が追加された時に、jsの処理が動くようにした
    $('.image_upload').on('change', function (e) {
    
    処理が始まると、プレビューのviewが追加される。
    $('#previewes').append(append_preview);
    
    追加されたviewは#previewsの末尾に追加されるので、:last-childで追加要素を指定して、attrで画像を入れている。
    reader.onload = function(e){
    　$('.preview:last-child').attr('src', e.target.result);
    }

--- app/assets/javascripts/product-new_images.js
+++ app/assets/javascripts/product-new_images.js
@@ -1,33 +1,86 @@
+
 $(document).on('turbolinks:load', function(){
-  var dropzone = $('.dropzone-area');
-  var dropzone2 = $('.dropzone-area2');
-  var appendzone = $('.upload-box');
+  var image_upload_area = $('.form-image__field');
+  var image_upload_area2 = $('.form-image__field2');
+  var label = $('.upload-label');
+  var previewes = $('#previewes')
+  var previewes2 = $('#previewes2');
   var images = [];
   var inputs = [];
   var input_area = $('.input_area');
-  var preview = $('.preview')
-  var preview2 = $('.preview2');
-  var append_preview = `<li.image-preview>
-                          <div class="image-preview_wapper">
-                            <img>
-                          </div>
-                          <div class="image-preview_btn"> 
-                            <a>"編集"</a>
-                            <a>"削除"</a>
-                          </div>
-                        </li>`
-
-  $(document).on('change','input[type="file"].hidden',function(e){
-    var file = $(this).prop('files')[0];
+  var new_image = $(`<input multiple="multiple" name="images[image][]" class="hidden" data-image="0" type="file" id="product_images_attributes_0_name"></input>`)
+  
+  // プレビュー機能
+  $('.image_upload').on('change', function (e) {
     var reader = new FileReader();
-    inputs.push($(this));
+    var append_preview = $(`<li class="image-preview">
+                              <div class="image-preview_wapper">
+                                <img class="preview">
+                              </div>
+                              <div class="image-preview_btn">
+                                <a href="/products/new">編集</a>
+                                <a href="/products/new">削除</a>
+                              </div>
+                              </li>`
+                          );                      
+    // var file = $(this).prop('files')[0];
+    // inputs.push($(this));
+    $('#previewes').append(append_preview);
     reader.onload = function(e){
-      append_preview.find('img').attr({
-        scr: e.target.result
-      })
+      $('.preview:last-child').attr('src', e.target.result);
     }
-    reader.readAsDataURL(file);
-    images.push(img);
-  })
+    reader.readAsDataURL(e.target.files[0]);
+    // images.push(append_preview);
+    console.log('OK2');
 
+
+    // // 画像が4枚以下のとき
+    // if (images.length <= 4) {
+    //   $('#previewes').empty();
+    //   $.each(images, function(index, image) {
+    //     image.data('image', index);
+    //     previewes.append(image);
+    //   })
+    //   image_upload_area.css({
+    //     'width': `calc(100% - (20% * ${images.length}))`
+    //   })
+
+    //   // 画像が5枚のとき
+    // } else if (images.length == 5) {
+    //   $("#previewes").empty();
+    //   $.each(images, function(index, image) {
+    //     image.data("image", index);
+    //     previewes.append(image);
+    //   });
+    //   image_upload_area.css({
+    //     display: "none"
+    //   });
+    //   image_upload_area2.css({
+    //     display: "block"
+    //   });
+    //   previewes2.empty();
+    //   // 画像が６枚以上のとき
+    // } else if (images.length >= 6) {
+    //   image_upload_area.css({
+    //     'display': 'none'
+    //   })
+    //   image_upload_area2.css({
+    //     'display': 'block'
+    //   })
+    //   $.each(images, function(index, image){
+    //     image.attr('data-image', index);
+    //     previewes2.append(image);
+    //     image_upload_area2({
+    //       'width': `calc(100% - (135px * ${images.lenght - 5}))`
+    //     });
+    //   });
+    // } else if (images.length == 10){
+    //   image_upload_area.css({
+    //     'display': 'none'
+    //   });
+    //   image_upload_area2.css({
+    //     'display': 'none'
+    //   });
+    // }
+  })
 });
