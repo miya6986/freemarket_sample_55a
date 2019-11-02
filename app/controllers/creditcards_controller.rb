1626cd8637cc02afcae474440e4609adf4d8dfb2
nousi <nousidayo@gmail.com>
2019-11-02 21:01:37 +0900

    トークン名の修正
    binding.pryでトークン名を確認すると、
    「　payjp-token　」ではなく、
    「　payjpToken　」であることがわかった。
    そのために、修正をした。
    
    また、render 'new'でなければエラーが表示されたため、修正している。

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -33,20 +33,20 @@ class CreditcardsController < ApplicationController
 
   def create
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    if params['payjp-token'].blank?
+    if params['payjpToken'].blank?
       redirect_to action: "index"
     else
     customer = Payjp::Customer.create(
       description: 'test',
       email: current_user.email,
-      card: params['payjp-token'],
+      card: params['payjpToken'],
       metadata: {user_id: current_user.id}
     )
     @card = Creditcard.new(user_id: current_user.id, customer_id: customer.id)
       if @card.save
-        redirect_to action: "index"
+        render 'index'
       else
-        redirect_to action: "new"
+        render 'new'
       end
     end
   end
