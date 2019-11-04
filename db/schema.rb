94cd7bc39733e52e657df8c737f837be655dc9c1
KengoNagata <majorboy47@gmail.com>
2019-11-05 06:43:47 +0900

    category_sizeモデル/テーブルを作成
    理由
    ・本アプリの仕様ではSizeは「洋服系(大人用」、「洋服系（子供用」、「靴（男性用）」、「靴（女性用」というように複数のパターンに分かれている。
    ・上記仕様実現のため、gem ancectryを利用する
    ・ancestryを使う場合、CategoryモデルとSizeモデルは多対多の関係になるため、中間テーブルも併せて作成することにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_213008) do
+ActiveRecord::Schema.define(version: 2019_11_04_213643) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2019_11_04_213008) do
     t.datetime "updated_at", null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry"
     t.index ["name"], name: "index_categories_on_name"
+  end
+
+  create_table "category_sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "size_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_sizes_on_category_id"
+    t.index ["size_id"], name: "index_category_sizes_on_size_id"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
