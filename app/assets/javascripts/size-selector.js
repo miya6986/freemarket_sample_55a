fad2a3f513fd3a44ff524d76e7a6cdcf0d12cac4
KengoNagata <majorboy47@gmail.com>
2019-11-06 18:46:48 +0900

    サイズのセレクトBox(js)→実装の枠組みを作成

--- app/assets/javascripts/size-selector.js
+++ app/assets/javascripts/size-selector.js
@@ -1,5 +1,19 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
+    // サイズセレクトボックスのオプション表示
+    function appendSizeOption(size){
+      var html = `<option value="${size.size}">${size.size}</option>`;
+      return html;
+    }
 
+    // サイズ・ブランド入力欄の表示作成
+    function appendSizeBox(insertHTML){
+
+    }
+
+    //孫カテゴリー選択後のイベント
+    $().on('change','',function(){
+
+    });
   });
 });
