08b13f64429e44a1fb7f170d118058b5c8602be1
nousi <nousidayo@gmail.com>
2019-11-06 14:35:38 +0900

    コメントアウトの解除

--- app/assets/javascripts/product-new_images.js
+++ app/assets/javascripts/product-new_images.js
@@ -79,6 +79,6 @@ $(document).on('turbolinks:load', function(){
   })
   //削除機能
   $(document).on('click',"image-preview_btn_delete",function(){
-    // var index = $('image-preview').index(this);
+    var index = $('image-preview').index(this);
   })
 });
