c3b41b7e786fcc92768052932355e43afe8571cd
miya6986 <miyadev49802@gmail.com>
2019-11-09 18:33:18 +0900

    Bug fix v2
    - Delete Unnecessary code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,26 +26,4 @@ class ProductsController < ApplicationController
     end
     @seller = @product.user.find(products_params[:seller_id])
   end
-
-  private
-
-  def products_params
-    params.require(:products).permit( :id,
-                                      :name,
-                                      :text,
-                                      :size,
-                                      :condition,
-                                      :postage,
-                                      :shipping_method,
-                                      :prefecture_id,
-                                      :shipping_days,
-                                      :price,
-                                      :buyer_id,
-                                      :seller_id,
-                                      :brand_id,
-                                      :category_id,
-                                      :user_id
-                                    )
-  end
-
 end
