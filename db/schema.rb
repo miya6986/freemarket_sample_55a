4db695ca57c6f913aaf92b87a6bdd7a94bbc561c
KengoNagata <majorboy47@gmail.com>
2019-11-10 06:46:27 +0900

    Productモデル→Brandのアソシエーションにoptional:trueを追加
    理由
    ・本アプリの仕様ではBrandは任意登録になっている
    ・Brand=nilの場合、belongs_toのアソシエーションでバリデーションエラーが出ないように設定した

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_11_04_213643) do
+
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "firstname", null: false
     t.string "lastname", null: false
