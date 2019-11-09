ec8cf04ea0b2d141276c035766b7b11a22a5199a
KengoNagata <majorboy47@gmail.com>
2019-11-09 12:32:50 +0900

    中間テーブル(category_sizes）のアソシエーションを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,6 @@ class Category < ApplicationRecord
   has_ancestry
   has_many :product_categories, dependent: :destroy
   has_many :products, through: :product_categories
+  has_many :category_sizes
   has_many :sizes, through: :category_sizes
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,4 +1,5 @@
 class Size < ApplicationRecord
   has_ancestry
+  has_many :category_sizes
   has_many :categories, through: :category_sizes
 end
