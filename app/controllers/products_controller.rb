6a63fb8c245a883f1a0492fd0f8b93afe4a5db04
KengoNagata <majorboy47@gmail.com>
2019-11-10 05:57:55 +0900

    コントローラ→get_sizeアクションの定義
    概要
    ・商品出品時の孫カテゴリ選択後、サイズ・ブランドのセレクトボックスが動的に生成（ajax)されるように、get_sizeアクションを定義した。
    ・また、Productモデルで別モデル（Brand)のデータが保存できるようにストロングパラメータに追記した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,8 +21,12 @@ class ProductsController < ApplicationController
     @grandchildren = Category.find("#{params[:child_id]}").children
   end
 
-
-
+  def get_size
+    selected_childid = Category.find("#{params[:grandchild_id]}").parent
+    if category_with_size = selected_childid.sizes[0]
+      @sizes = category_with_size.children
+    end
+  end
 
   def create
     @product = Product.new(product_params)
@@ -49,6 +53,7 @@ class ProductsController < ApplicationController
       :shipping_days,
       :price,
       images_attributes: [:name],
+      brand_attributes: [:name],
       category_ids: []
     )
     # .merge(seller_id: current_user.id)
