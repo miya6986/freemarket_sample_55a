f81b23febbdfbc61507bc5342f68b2d77751cee9
KengoNagata <majorboy47@gmail.com>
2019-11-09 09:52:26 +0900

    Categoryモデル、Sizeモデル　多対多のアソシエーションを定義

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_ancestry
   has_many :product_categories, dependent: :destroy
   has_many :products, through: :product_categories
+  has_many :sizes, through: :category_sizes
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,3 +1,4 @@
 class Size < ApplicationRecord
   has_ancestry
+  has_many :categories, through: :category_sizes
 end
