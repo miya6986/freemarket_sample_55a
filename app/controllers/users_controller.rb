247ac837be8836d9bb4f94ac21e94909a66bae4b
miya6986 <miyadev49802@gmail.com>
2019-11-09 22:09:59 +0900

    Correct users_controller
    - @products parameter fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def my_selling_products
     @user = User.find(current_user.id)
-    @products = @user.products.includes(:users).where(id: current_user.id).order(id: "DESC").limit(15)
+    @products = @user.products.includes(:product).where(seller_id: current_user.id).order(id: "DESC").limit(15)
   end
 
 end
