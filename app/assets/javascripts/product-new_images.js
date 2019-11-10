f5c5df94fc4c81b78716ee31f926028636b2425f
nousi <nousidayo@gmail.com>
2019-11-11 01:00:58 +0900

    以前のproduct-new_images.jsをほとんど削除
    やり方が全く異なるため削除。
    
    新しい動作の詳細は、
    コメントアウトで記述している。
    
    プレビュー機能と削除機能を実装している。
    ただし、input class="image_upload" type="file" は何もCSSを効かせていない。

--- app/assets/javascripts/product-new_images.js
+++ app/assets/javascripts/product-new_images.js
@@ -1,84 +1,40 @@
 
-$(document).on('turbolinks:load', function(){
-  var image_upload_area = $('.form-image__field');
-  var image_upload_area2 = $('.form-image__field2');
-  var label = $('.upload-label');
-  var previews = $('#previews')
-  var previews2 = $('#previews2');
-  var images = [];
-  var inputs = [];
-  var input_area = $('.input_area');
-  var new_image = $(`<input multiple="multiple" name="images[image][]" class="hidden" data-image="0" type="file" id="product_images_attributes_0_name"></input>`)
-  
+$(document).on('turbolinks:load', function(){  
   // プレビュー機能
-  $('.image_upload').on('change', function (e) {
-    var reader = new FileReader();
-    var append_preview = $(`<li class="image-preview">
-                              <div class="image-preview__wapper">
-                                <img class="preview">
-                              </div>
-                              <div class="image-preview_btn">
-                                <div class="image-preview_btn_edit">編集</div>
-                                <div class="image-preview_btn_delete">削除</div>
-                              </div>
-                            </li>`
-                          );   
-    // 画像ファイルを読み込む
-    reader.readAsDataURL(e.target.files[0]);
-    //画像ファイルが読み込んだら、処理が実行される。
-    reader.onload = function(e){
-      $(append_preview).find('.preview').attr('src', e.target.result);
-    }
-    //画像ファイルをimagesに保存する。
-    images.push(append_preview);
-    if (images.length <= 4) {
-      $('#previews').empty();
-      $.each(images,function(index, image){
-        image.data('image', index);
-        $('#previews').append(image)
-      })
-      image_upload_area.css({
-        'width': `calc(100% - (20% * ${images.length}))`
-      })
-    }
-    else if (images.length == 5) {
-      $('#previews').empty();
-      $.each(images,function(index, image){
-        image.data('image', index);
-        $('#previews').append(image)
-      })
-      image_upload_area.css({
-        'display': 'none'
-      })
-      image_upload_area2.css({
-        'display': 'block'
-      })
-    }
-    else if (images.length >= 6) {
-      var images_0_5 = images.slice(0,5);
-      var images_6_10 = images.slice(5);
-      $('#previews').empty();
-      $('#previews2').empty();
-      $.each(images_0_5,function(index, image){
-        image.data('image', index);
-        $('#previews').append(image)
-      })
-      $.each(images_6_10,function(index, image){
-        image.data('image', index);
-        $('#previews2').append(image)
-      })
-      image_upload_area2.css({
-        'width': `calc(100% - (20% * ${images.length - 5}))`
-      })
-      if (images.length == 10) {
-        image_upload_area2.css({
-          'display': 'none'
-        })
+  //'change'イベントでは$(this)で要素が取得できないため、 'click'イベントを入れた。
+  //これにより$(this)で'input'を取得することができ、inputの親要素である'li'まで辿れる。
+  $(document).on('click', '.image_upload', function(){
+    $input = $(this);
+    $li = $(this).parent('.image-preview');
+    $ul = $li.parent('#previews');
+    $('.image_upload').on('change', function (e) {
+      //inputで選択した画像を読み込む
+      var reader = new FileReader();
+      //$liに追加するためのプレビュー画面
+      var preview = $('<div class="image-preview__wapper"><img class="preview"></div><div class="image-preview_btn"><div class="image-preview_btn_edit">編集</div><div class="image-preview_btn_delete">削除</div></div>'); 
+      //次の画像を読み込むためのinput。処理の最後にappendで追加する。 
+      var append_input = $(`<li class="image-preview"><input class="image_upload" type="file" name="product[images_attributes][0][name]" id="product_images_attributes_0_name">`)
+      
+
+      // プレビューに追加させるために、inputから画像ファイルを読み込む。
+      reader.readAsDataURL(e.target.files[0]);
+      //画像ファイルが読み込んだら、処理が実行される。 
+      reader.onload = function(e){
+        //previewをappendで追加する前に、プレビューできるようにinputで選択した画像を<img>に'src'で付与させる
+        $(preview).find('.preview').attr('src', e.target.result);
       }
-    }
+      //inputの画像を付与した,previewを$liに追加。
+      $li.append(preview);
+      //プレビュー完了後は、inputを非表示にさせる。これによりプレビューだけが残る。
+      $input.css('display','none');
+      //"ul"に新しい"li(inputボタン)"を追加させる。
+      $ul.append(append_input);
+    })
   })
   //削除機能
-  $(document).on('click',"image-preview_btn_delete",function(){
-    var index = $('image-preview').index(this);
+  $(document).on('click','.image-preview_btn_delete',function(){
+    $li = $(this).parents('.image-preview');
+    //"li"ごと削除して、previewとinputを削除させる。
+    $li.remove();
   })
 });
