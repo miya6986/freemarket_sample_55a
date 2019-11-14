157b42130d7f0768be283233693092f8f0c7e6d4
nousi <nousidayo@gmail.com>
2019-11-15 00:56:41 +0900

    解説用のコメントアウトを削除

--- app/assets/javascripts/Payjp.js
+++ app/assets/javascripts/Payjp.js
@@ -1,5 +1,4 @@
 $(document).on('turbolinks:load',function(){
-  /*公開キーのセット*/ 
   Payjp.setPublicKey('pk_test_62c91b0f0f85efaf575ea6b5');
   var form = $(".form"),
       number = form.find('input[name="card_number"]'),
@@ -9,9 +8,7 @@ $(document).on('turbolinks:load',function(){
 
 
   $("#charge-form").click(function() {
-    /* submitの送信を止めて、Jqueryで指定の処理をさせる。*/
     form.find("input[type=submit]").prop("disabled", true);
-    /* Payjpに送るためのトークン情報を入力欄から取得していく*/
     var card = {
       number: $("#card_number").val(),
       cvc: $("#cvc").val(),
@@ -19,24 +16,18 @@ $(document).on('turbolinks:load',function(){
       exp_year: $("#exp_year").val(),
     };
     
-    /*取得した情報からPayjp指定の処理でトークンを生成する*/
     Payjp.createToken(card, function(status, response) {
-      /*エラーがあった場合、下記の処理が動く*/
       if (response.error){
         form.find('.payment-errors').text(response.error.message);
         form.find('button').prop('disabled', false);
       }   
-      /*エラーがない場合、処理が動く*/
       else {
-        /* Payjp用のトークン作成後は、dbには必要がないためクレジットカード情報は除去*/
         $("#card_number").removeAttr("name");
         $("#cvc").removeAttr("name");
         $("#exp_month").removeAttr("name");
         $("#exp_year").removeAttr("name");
         var token = response.id;
-        /* 隠しinputを入れる。*/
         form.append($('<input type="hidden" name="payjpToken" />').val(token));
-        /*止めていたsubmitを動かして、Payjp,dbに保存する。*/
         form.get(0).submit();
       };
     });
