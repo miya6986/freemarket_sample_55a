ddaad4abab442fcc85460fb112be4c8c507f9f64
yutaro704 <yutaro.704@gmail.com>
2019-11-15 00:01:01 +0900

    不要なインスタンスメソッドの削除
    コントローラーに記述していた不要なインスタンスメソッドを削除し、viewファイル側に記述をした。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,32 +9,8 @@ class ProductsController < ApplicationController
   end
 
   def show
-    # トップページから詳細ページへの移管を行う
     @product = Product.find(params[:id])
-    # 出品者
-    @seller = @product.seller.nickname
-    # アイテム名
-    @name = @product.name
-    # 商品画像
     @image = @product.images.first.name.to_s
-    # カテゴリー
-    # @category = Category.all.order("id").limit(3)
-    # ブランド
-    @brand = @product.brand.name
-    # 状態
-    @condition = @product.condition
-    # 配送料の負担
-    @postage = @product.postage
-    # 配送の方法
-    @shipping_method = @product.shipping_method
-    # 配送元の地域
-    # @prefecture = @product.prefecture_id
-    # 配送の目安
-    @shipping_days = @product.shipping_days
-    # 値段
-    @price = @product.price
-    # 商品の説明
-    @detail = @product.description
   end
   
 end
