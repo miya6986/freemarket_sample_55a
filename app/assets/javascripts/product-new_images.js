9a2b6412a104aaf3c5da1387af673166aee0aa03
nousi <nousidayo@gmail.com>
2019-11-15 02:13:55 +0900

    削除時の動作を追加
    現時点において、うまく動作ができるわけではないが、10枚目を削除して9枚目になると、
    
    新しいinputをappendして追加される。
    それ以外のときは、長さ調整のif文だが、これがうまく行っていない。

--- app/assets/javascripts/product-new_images.js
+++ app/assets/javascripts/product-new_images.js
@@ -76,10 +76,31 @@ $(document).on('turbolinks:load', function(){
     var append_input = $(`<li class="image-preview input"><label class="upload-label"><div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード<div class="input-area"><input class="hidden image_upload" type="file"></div></div></label></li>`)
     $ul = $('#previews')
     $li = $(this).parents('.image-preview');
-    $lis = $ul.find('.image-preview');
-    $label = $ul.find('.input')
     //"li"ごと削除して、previewとinputを削除させる。
     $li.remove();
+    $lis = $ul.find('.image-preview');
+    $label = $ul.find('.input');
+    if($lis.length <= 4 ){
+      $('#previews li:last-child').css({
+        'width': `calc(100% - (20% * ${$lis.length}))`
+      })
+    }
+    else if($lis.length == 5 ){
+      $('#previews li:last-child').css({
+        'width': `100%`
+      })
+    }
+    else if($lis.length < 9 ){
+      $('#previews li:last-child').css({
+        'width': `calc(100% - (20% * (${$lis.length} - 5 )))`
+      })
+    }
+    else if($lis.length == 9 ){
+      $ul.append(append_input)
+      $('#previews li:last-child').css({
+        'width': `calc(100% - (20% * (${$lis.length} - 5 )))`
+      })
+    }
   })
 });
 
