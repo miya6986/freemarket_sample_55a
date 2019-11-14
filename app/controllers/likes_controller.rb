786c363810319a71d8931c2f686ccbea1c02a066
nousi <nousidayo@gmail.com>
2019-11-15 03:33:02 +0900

    コントローラーにインスタンス変数を定義
    @liks = current_user.likes.order(id: "DESC")
    indexはマイページでいいね！の一覧を表示させるので、自分のいいね！一覧を取得する必要がある。
    そのためのインスタンス変数
    
      def create
        @like = current_user.likes.create(product_id: params[:product_id])
        redirect_back(fallback_location: root_path)
      end
    
    redirect_back(fallback_location: root_path)は元の画面に戻るが、うまく行かない場合はroot_pathに戻るためのメソッド

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,16 @@
 class LikesController < ApplicationController
   def index
+    @liks = current_user.likes.order(id: "DESC")
   end
 
   def create
+    @like = current_user.likes.create(product_id: params[:product_id])
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @like = Like.find_by(product_id: params[:product_id], user_id: current_user.id)
+    @like.destroy
+    redirect_back(fallback_location: root_path)
   end
 end
