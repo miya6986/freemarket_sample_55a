b4afa8531ea498bd741e33e2c4d869caac840d67
nousi <nousidayo@gmail.com>
2019-11-17 05:50:22 +0900

    Product#buy　売り切れ時は入れないようにする
    if @product.buyer.blank? 購入者いないとき、表示
    else
          redirect_back(fallback_location: root_path)
    　　前の画面に戻す。戻せない場合はroot_pathに戻す

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,34 +48,37 @@ class ProductsController < ApplicationController
 
   def buy
     @product = Product.find(params[:id])
-    @address = current_user.address
-    @address_full = "#{@address.prefecture.name}#{@address.city_name}#{@address.address_number}#{@address.building_name}"
-    @full_name = "#{@address.firstname} #{@address.lastname}"
-    @postalcode = @address.postalcode
-    @card = Creditcard.where(user_id: current_user.id).first if Creditcard.where(user_id: current_user.id).present?
-    if @card.present?
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @card_info = customer.cards.retrieve(customer.default_card)
-      @card_brand = @card_info.brand
-      @exp_month = @card_info.exp_month.to_s
-      @exp_year = @card_info.exp_year.to_s.slice(2,3) 
-      case @card_brand
-      when "Visa"
-        @card_image = "visa.svg"
-      when "JCB"
-        @card_image = "jcb.svg"
-      when "MasterCard"
-        @card_image = "master-card.svg"
-      when "American Express"
-        @card_image = "american_express.svg"
-      when "Diners Club"
-        @card_image = "dinersclub.svg"
-      when "Discover"
-        @card_image = "discover.svg"
+    if @product.buyer.blank?
+      @address = current_user.address
+      @address_full = "#{@address.prefecture.name}#{@address.city_name}#{@address.address_number}#{@address.building_name}"
+      @full_name = "#{@address.firstname} #{@address.lastname}"
+      @postalcode = @address.postalcode
+      @card = Creditcard.where(user_id: current_user.id).first if Creditcard.where(user_id: current_user.id).present?
+      if @card.present?
+        Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+        customer = Payjp::Customer.retrieve(@card.customer_id)
+        @card_info = customer.cards.retrieve(customer.default_card)
+        @card_brand = @card_info.brand
+        @exp_month = @card_info.exp_month.to_s
+        @exp_year = @card_info.exp_year.to_s.slice(2,3) 
+        case @card_brand
+        when "Visa"
+          @card_image = "visa.svg"
+        when "JCB"
+          @card_image = "jcb.svg"
+        when "MasterCard"
+          @card_image = "master-card.svg"
+        when "American Express"
+          @card_image = "american_express.svg"
+        when "Diners Club"
+          @card_image = "dinersclub.svg"
+        when "Discover"
+          @card_image = "discover.svg"
+        end
       end
+    else
+      redirect_back(fallback_location: root_path)
     end
-
   end
 
   def search
