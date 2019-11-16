5a1e89216c67a7f7588d270adf10e0084a80d1f5
miya6986 <miyadev49802@gmail.com>
2019-11-17 00:51:03 +0900

    Add no cache settings
    - Add delete sessions when back to step1

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,14 @@ class RegistrationsController < ApplicationController
   
   before_action :validates_step2, only: :step3
   before_action :validates_step3, only: :step4
+  before_action :set_cache_buster
 
   def step1
+    session.delete(:nickname)
+    session.delete(:email)
+    session.delete(:password_token)
+    session.delete(:uid)
+    session.delete(:provider)
   end 
 
   def step2
@@ -185,5 +191,11 @@ class RegistrationsController < ApplicationController
       )
     end
     return user
+  end
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
