5f2b8cad6a03912baaf0e4e254d0858c50599164
miya6986 <miyadev49802@gmail.com>
2019-11-16 10:39:34 +0900

    Update schema.rb
    - add two tables sizes, category_sizes

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2019_11_09_235751) do
     t.index ["name"], name: "index_categories_on_name"
   end
 
+  create_table "category_sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "size_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_sizes_on_category_id"
+    t.index ["size_id"], name: "index_category_sizes_on_size_id"
+  end
+
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "product_id", null: false
@@ -81,6 +90,14 @@ ActiveRecord::Schema.define(version: 2019_11_09_235751) do
     t.index ["buyer_id"], name: "index_products_on_buyer_id"
     t.index ["name"], name: "index_products_on_name"
     t.index ["seller_id"], name: "index_products_on_seller_id"
+  end
+
+  create_table "sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "kind", null: false
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_sizes_on_ancestry"
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
