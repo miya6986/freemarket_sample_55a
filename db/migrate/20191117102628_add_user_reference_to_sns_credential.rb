d727d0ed86b7a7eb91e794a5ebb9b84f79ab7960
miya6986 <miyadev49802@gmail.com>
2019-11-17 21:31:00 +0900

    fix mistype in migration files

--- db/migrate/20191117102628_add_user_reference_to_sns_credential.rb
+++ db/migrate/20191117102628_add_user_reference_to_sns_credential.rb
@@ -1,5 +1,5 @@
 class AddUserReferenceToSnsCredential < ActiveRecord::Migration[5.2]
   def change
-    add_reference :sns_credential, :user, null: false, foreign_key: true
+    add_reference :sns_credentials, :user, null: false, foreign_key: true
   end
 end
