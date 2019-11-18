46757d8e29c85aa99e80fae96d2e1f6ef2f387a7
nousi <nousidayo@gmail.com>
2019-11-18 17:01:14 +0900

    マイページ本人確認ページのパンくずリスト表示
    Creditcards#indexのパンくずリストがプロフィールになっていたので、修正。
    
    その後、下記を作成
    
    crumb :user_identification_users do
      link "本人情報の登録", user_identification_users_path
      parent :mypage
    end

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -33,7 +33,12 @@ crumb :profile do
 end
 
 crumb :creditcards_index do
-  link "プロフィール", creditcards_path
+  link "支払い方法", creditcards_path
+  parent :mypage
+end
+
+crumb :user_identification_users do
+  link "本人情報の登録", user_identification_users_path
   parent :mypage
 end
 
@@ -41,28 +46,3 @@ crumb :logout do
   link "ログアウト", logout_path
   parent :mypage
 end
-
-# crumb :projects do
-#   link "Projects", projects_path
-# end
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
