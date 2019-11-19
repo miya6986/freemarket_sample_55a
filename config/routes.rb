7ae5946ebb6bdb8c1bf701a754514449f70399b1
miya6986 <miyadev49802@gmail.com>
2019-11-19 18:38:39 +0900

    delete unnecessary space in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   resources :creditcards, only: [:index, :new, :create, :destroy]
 
   resources :products do
-
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
