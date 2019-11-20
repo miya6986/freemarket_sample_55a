d4a520de11091c8a0625bd66a5eb971d618e7bca
nousi <nousidayo@gmail.com>
2019-11-20 20:04:29 +0900

    likes_controller.rb：create, destroy, index
    createとdestroyを設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,21 @@
 class LikesController < ApplicationController
   def index
+    @likes = current_user.likes
   end
 
   def create
+    @like = Like.create(user_id: current_user.id, product_id: @product.id)
+    redirect_back(fallback_location: product_path(@product))
   end
 
   def destroy
+    @like = Like.find_by(user_id: current_user.id, product_id: @product.id)
+    @like.destroy
+    redirect_back(fallback_location: product_path(@product))
   end
+
+  private
+  def set_product
+    @product = Product.find(params[:Product_id])
+  end 
 end
