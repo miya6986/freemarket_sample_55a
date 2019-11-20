b8ff7ef786a56dfa94f02573fe46ecc1123d8531
nousi <nousidayo@gmail.com>
2019-11-20 22:09:19 +0900

    @product = Product.find(params[:product_id])を定義
    show.html.hamlにある
    = button_to likes_path(product_id: @product.id)の
    (product_id: @product.id)がparams[:product_id]となって、コントローラーに渡される。
    
    それを元にコントローラーで@productを定義している。
    @product = Product.find(params[:product_id])

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,16 @@ class LikesController < ApplicationController
   end
 
   def create
+    @product = Product.find(params[:product_id])
     @like = Like.create(user_id: current_user.id, product_id: @product.id)
-    redirect_back(fallback_location: product_path(@product))
+    redirect_back(fallback_location: product_path(@product.id))
   end
 
   def destroy
+    @product = Product.find(params[:product_id])
     @like = Like.find_by(user_id: current_user.id, product_id: @product.id)
     @like.destroy
-    redirect_back(fallback_location: product_path(@product))
+    redirect_back(fallback_location: product_path(params[:product_id]))
   end
 
   private
