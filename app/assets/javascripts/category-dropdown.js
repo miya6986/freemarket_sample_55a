4834b9a002412530591dde16badc07d83ab73cb2
KengoNagata <majorboy47@gmail.com>
2019-11-20 20:14:44 +0900

    header → カテゴリーのドロップダウンメニューを作成（親・子・孫カテゴリー表示可能 ）
    補足
    ・変数名、クラス名についてより分かりやすい名前になるようにリファクタリングを行った。
    
    ・チーム内コードレビュー補助用でコメントアウトも追加した

--- app/assets/javascripts/category-dropdown.js
+++ app/assets/javascripts/category-dropdown.js
@@ -1,17 +1,42 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
     var menu = $(".menu-left__item:first-child")
-    var category = $("#category");
+    var parent = $(".parent__item");
+    var child = $(".child__item")
+    var parentCategory = $(".parent");
+    var childCategory = $(".child")
+    var grandchildCategory = $(".grandchild")
 
-    //初期状態ではカテゴリーメニュー非表示
-    category.hide();
-    // 「カテゴリーから探す」 hover時にメニューを表示
+    //初期状態では各カテゴリーメニューを非表示にしておく
+    parentCategory.hide();
+    childCategory.hide();
+    grandchildCategory.hide();
+    
+    //「カテゴリーから探す」 hover時に親カテゴリーメニューを表示
     menu.hover(function(){
-      category.show();
+      parentCategory.show();
     },
-    // hoverが外れたら再度メニューを非表示にする
+    // hoverが外れたら再度親カテゴリーメニューを非表示にする
     function(){
-      category.hide();
+      parentCategory.hide();
+    });
+
+    //親カテゴリー hover時に該当の子カテゴリーメニューを表示する
+    $(parent).hover(function(){
+      $(this).find(childCategory).show();
+    },
+    // hoverが外れたら再度子カテゴリーメニューを非表示にする
+    function(){
+      $(this).find(childCategory).hide();
+    });
+
+    //子カテゴリー hover時に該当の孫カテゴリーメニューを表示する
+    $(child).hover(function(){
+      $(this).find(grandchildCategory).show();
+    },
+    // hoverが外れたら再度孫カテゴリーメニューを非表示にする
+    function(){
+      $(this).find(grandchildCategory).hide();
     });
   });
 });
