ceb496ad5900bdfb17aed623a2385a9c7c4212c8
KengoNagata <majorboy47@gmail.com>
2019-11-22 21:40:16 +0900

    サイズ編集機能のjsを実装

--- app/assets/javascripts/size-selector.js
+++ app/assets/javascripts/size-selector.js
@@ -53,7 +53,39 @@ $(document).on('turbolinks:load', function() {
         $('#brand').remove();
       }
     });
+  });
+});
 
+
+//編集機能
+$(document).on('turbolinks:load', function() {
+  var categoryBox = $('.form-details__form-box__category')
+
+  $(function(){
+    // サイズセレクトボックスのオプション表示
+    function appendSizeOption(size){
+      var html = `<option value="${size.kind}">${size.kind}</option>`;
+      return html;
+    }
+
+    // サイズ・ブランド入力欄の表示作成
+    function appendSizeBox(insertHTML){
+      var sizeSelectHtml = '';
+      sizeSelectHtml = `<div id="size">
+                          <label class="label">サイズ</label>
+                          <span>必須</span>
+                          <div class='form-select'>
+                          <select class= 'select-default' name="product[size]">
+                            <option value data-category="---">---</option>
+                            ${insertHTML}
+                          </select>
+                           <i class='fa fa-angle-down icon-angle-down'></i>
+                          </div>
+                        </div>`
+      categoryBox.append(sizeSelectHtml);
+    }
+
+    //孫カテゴリー選択後のイベント
     categoryBox.on('change','#grandchild-category',function(){
       var grandchildCategory = $('#grandchild-category option:selected').data('category'); 
       if (grandchildCategory != "---"){ 
