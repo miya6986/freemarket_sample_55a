381033acfe3fdcf434a895476c3e516452c44de5
nousi <nousidayo@gmail.com>
2019-11-22 14:54:36 +0900

    複数画像の削除用のjsを作成
    一つのjsで機能をまとめていくと、意図しない動作が起こることがあるため、作成

---
+++ app/assets/javascripts/product-image-delete.js
@@ -0,0 +1,35 @@
+$(document).on('click','.image-preview_btn_delete',function(){
+  var append_input = $(`<li class="image-preview input"><label class="upload-label"><div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード<div class="input-area"><input class="hidden image_upload" type="file"></div></div></label></li>`)
+  $ul = $('#previews')
+  $lis = $ul.find('.image-preview');
+  $input = $ul.find('.input');
+  var append_input = $(`<li class="image-preview input"><label class="upload-label"><div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード<div class="input-area"><input class="hidden image_upload" type="file"></div></div></label></li>`)
+  $ul = $('#previews')
+  $li = $(this).parents('.image-preview');
+  //"li"ごと削除して、previewとinputを削除させる。
+  $li.remove();
+  $lis = $ul.find('.image-preview');
+  $label = $ul.find('.input');
+  if($lis.length <= 4 ){
+    $('#previews li:last-child').css({
+      'width': `calc(100% - (20% * ${$lis.length}))`
+    })
+  }
+  else if($lis.length == 5 ){
+    $('#previews li:last-child').css({
+      'width': `100%`
+    })
+  }
+  else if($lis.length <= 9 ){
+    $('#previews li:last-child').css({
+      'width': `calc(100% - (20% * (${$lis.length} - 5 )))`
+    })
+  }
+  else if($lis.length == 10 ){
+    $ul.append(append_input)
+    $('#previews li:last-child').css({
+      'width': `calc(100% - (20% * (${$lis.length} - 5 )))`
+    })
+  }
+});
+
