aea9e804dd747df1bcbb13f70cb6d73981501d0b
KengoNagata <majorboy47@gmail.com>
2019-11-24 15:30:56 +0900

    カテゴリー詳細表示ページ→ページネーションを導入
    [内容]
    ・１０件ごとにページが切り替わるように設定した。
    ・検索機能の部分テンプレートを活用して実装した。
    ・category_controllerではN+1問題対策としてsellerとimage を事前に読み込むようにした

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,5 +4,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @products = @category.products.page(params[:page]).per(10).includes(:seller,:images)
   end
 end
