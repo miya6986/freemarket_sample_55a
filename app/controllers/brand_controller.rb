b2573dc6c6534b15b606fcf425d3da9314ceeee4
nousi <nousidayo@gmail.com>
2019-11-27 16:53:12 +0900

    brand_controllerにjsonを追加
    product#newでbrand検索するために記述

--- app/controllers/brand_controller.rb
+++ app/controllers/brand_controller.rb
@@ -6,5 +6,9 @@ class BrandController < ApplicationController
   end
 
   def search
+    @brands = Brand.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    respond_to do |format|
+      format.html
+      format.json
   end
 end
