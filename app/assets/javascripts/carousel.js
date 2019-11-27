e601abbda5e78108fed6b5ee87e1b92e5ec75e09
yutaro704 <yutaro.704@gmail.com>
2019-11-28 00:55:18 +0900

    複数画像の修正
    可読性を上げるためにリファクタリング

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -2,18 +2,18 @@ $(document).on('turbolinks:load', function() {
   $(function(){
     var slider = ".slider"; // スライダー
     var thumbnailItem = ".thumbnail-list .thumbnail-item"; // サムネイル画像アイテム
-    
+    var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");　//「data-slick-index」(slickで設定したスライドに追加される属性)を取得。
+    var thumnailSelected = thumbnailItem+'[data-index="'+index+'"]' //選ばれたサムネイルを属性値「data-index」(slickで設定したスライドに追加される属性)に変更。
+
     // サムネイル画像アイテムに data-index でindex番号を付与
     $(thumbnailItem).each(function(){
     var index = $(thumbnailItem).index(this);
     $(this).attr("data-index",index);
     });
     
-    // スライダー初期化後、カレントのサムネイル画像にクラス「thumbnail-current」を付ける
-    // 「slickスライダー作成」の前にこの記述は書いてください。
+    // スライダー初期化(init)後に発火、カレントのサムネイル画像にクラス「thumbnail-current」を付ける
     $(slider).on('init',function(slick){
-    var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
-    $(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
+    $(thumnailSelected).addClass("thumbnail-current");
     });
 
     //slickスライダー初期化  
@@ -22,6 +22,7 @@ $(document).on('turbolinks:load', function() {
       infinite: false
     });
     //サムネイル画像アイテムをクリックしたときにスライダー切り替え
+    // slickGoTo = 指定したindex番号のスライドをカレントにする。
     $(thumbnailItem).on('mouseover',function(){
       var index = $(this).attr("data-index");
       $(slider).slick("slickGoTo",index,false);
