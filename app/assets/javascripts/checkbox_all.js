db17cdfebe965e83462704e4e02f67e42ae1a317
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-11-27 21:13:17 +0900

    配送料の負担にもチェックボックス全選択を作成
    詳細：
    全選択のjsに関して同じクラスを指定していると商品状態のチェックも入ってしまうため名前を差別化した。

--- app/assets/javascripts/checkbox_all.js
+++ app/assets/javascripts/checkbox_all.js
@@ -2,4 +2,12 @@ $(function() {
   $('.all').on("click",function(){
     $('.list').prop("checked", $(this).prop("checked"));
   });
+  $('.all-postage').on("click",function(){
+    $('.list-postage').prop("checked", $(this).prop("checked"));
+  });
 });
+// $(function() {
+//   $('.all-postage').on("click",function(){
+//     $('.list-postage').prop("checked", $(this).prop("checked"));
+//   });
+// });
