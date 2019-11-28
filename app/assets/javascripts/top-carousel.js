3299b238767a6145a56c8ebf5c8e439d570c811f
nousi <nousidayo@gmail.com>
2019-11-28 23:55:48 +0900

    Revert "不要なコメントアウトを削除"
    This reverts commit 6fa4e1c7707017eefa833dcd0b914335a8ef9dec.

--- app/assets/javascripts/top-carousel.js
+++ app/assets/javascripts/top-carousel.js
@@ -4,6 +4,7 @@ $(document).on('turbolinks:load',function(){
     autoplaySpeed: 4000,
     speed: 800,
     dots:     true,
+    // dotsClass: 'top-dot-class',
     infinite: true,
     prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
     nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
