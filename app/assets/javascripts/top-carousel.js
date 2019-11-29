6961ae426e83223dd9c5d555ff3bd9b2422ec8ca
nousi <nousidayo@gmail.com>
2019-11-29 17:30:34 +0900

    トップページのスライダー用のjsを復旧
    誤ったマージのため、masterが２週間以上前の状態に戻ってしまった。
    riverでも改善することができなかったため、
    一番新しい状態であった,brand_serachブランチのフォルダをそのまま移行した。
    
    それにより、masterは復旧したが、brand_searchブランチに含まれていない、トップページのスライダーの記述の復旧をしている。
    
    フォルダを上書きした際に消したjsファイルを再度作成している

---
+++ app/assets/javascripts/top-carousel.js
@@ -0,0 +1,11 @@
+$(document).on('turbolinks:load',function(){ 
+  $('.top-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 800,
+    dots:     true,
+    infinite: true,
+    prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+    nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+  });
+});
